14621c80ce7b009b8fd13991b4376ca23fbbffb2
cyberhoward <cyberhoward@protonmail.com>
2023-07-11 21:51:57 +0200

    add allow(unused)

--- contracts/dca/src/contract.rs
+++ contracts/dca/src/contract.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use crate::{
     error::AppError,
     handlers,
--- contracts/dca/src/handlers/instantiate.rs
+++ contracts/dca/src/handlers/instantiate.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use cosmwasm_std::{DepsMut, Empty, Env, MessageInfo, Response};
 
 use crate::contract::{AppResult, DCAApp};
